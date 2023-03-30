81dbd744a9efcab422d74e9897e350ece68a69dc
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-03-30 17:08:32 -0500

    auth

---
+++ FrontV2/lib/services/ath.dart
@@ -0,0 +1,21 @@
+import 'package:local_auth/local_auth.dart';
+
+class LocalAuth {
+  static final _auth = LocalAuthentication();
+
+  static Future<bool> _canAuthenticate() async =>
+      await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
+
+  static Future<bool> authenticate() async {
+    try {
+      if (!await _canAuthenticate()) return false;
+
+      return await _auth.authenticate(
+          localizedReason: 'Utilizar huella para iniciar sesión',
+          options: const AuthenticationOptions(
+              useErrorDialogs: true, stickyAuth: true));
+    } catch (e) {
+      return false;
+    }
+  }
+}
