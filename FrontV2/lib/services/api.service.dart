b8b63f00b0e211ca18bd1378be0398876e699943
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-03-30 16:51:31 -0500

    comentando el codigo

--- FrontV2/lib/services/api.service.dart
+++ FrontV2/lib/services/api.service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import '../exports.dart';
 import 'package:http/http.dart' as http;
 
-class APIservice{
+class APIservice {
   static const headers = {
     "Access-Control-Allow-Origin": "*",
     'Content-Type': 'application/json',
@@ -27,12 +27,19 @@ class APIservice{
     }
   }
 
+  //indica si el token es válido o no.
   static Future<bool> isValidToken(String token) async {
     final url = Uri.http(Config.apiURL, Config.isValidTokenAPI);
+
     try {
+      // Realiza una solicitud HTTP POST a la URL definida anteriormente. Incluye el cuerpo de la solicitud, que es un objeto JSON que contiene el `token`.
       final response = await http
           .post(url, headers: headers, body: jsonEncode({'token': token}))
-          .timeout(const Duration(seconds: 3));
+          .timeout(
+            const Duration(seconds: 3),
+          ); // Establece un tiempo de espera máximo de 3 segundos para la respuesta.
+
+      // Verifica si la respuesta tiene un código de estado HTTP 200 (OK). Si es así, devuelve `true`. De lo contrario, devuelve `false`.
       if (response.statusCode == 200) {
         return true;
       } else {
@@ -44,25 +51,39 @@ class APIservice{
   }
 
   static Future<int> biometricRegister(String email, String password) async {
+    // Obtiene el token de autenticación del almacenamiento compartido.
     final token = Shared.getToken();
     if (token == 'default') {
+      // Si el token es el valor predeterminado, devuelve 0 para indicar un error.
       return 0;
     }
+
     final url = Uri.http(Config.apiURL, Config.registerBiometric);
+
     final body =
         json.encode({"email": email, "password": password, "token": token});
+
     try {
+      // Realiza una solicitud HTTP POST a la URL definida anteriormente. Incluye el cuerpo de la solicitud, que es el objeto JSON construido anteriormente.
       final response = await http
           .post(url, headers: headers, body: body)
-          .timeout(const Duration(seconds: 5));
+          .timeout(
+            const Duration(seconds: 5),
+          ); // Establece un tiempo de espera máximo de 5 segundos para la respuesta.
+
+      // indica el resultado de la operación de registro biométrico.
       if (response.statusCode == 200) {
+        //almacena el token biométrico en el almacenamiento seguro y devuelve que el registro se ha realizado correctamente.
         await SecureStorageService.setBiometricToken(jsonDecode(response.body));
         return 1;
       } else if (response.statusCode == 402) {
+        // Si la respuesta tiene un código de estado HTTP 402, devuelve 2 para indicar que el usuario no está suscrito a la función de autenticación biométrica.
         return 2;
       } else if (response.statusCode == 409) {
+        // Si la respuesta tiene un código de estado HTTP 409, devuelve 3 para indicar que ya hay un usuario registrado con la dirección de correo electrónico proporcionada.
         return 3;
       } else {
+        // En cualquier otro caso, devuelve 4 para indicar un error.
         return 4;
       }
     } catch (e) {
@@ -71,18 +92,28 @@ class APIservice{
   }
 
   static Future<bool> removeBiometric() async {
+    // Obtenemos el token actual
     final token = Shared.getToken();
+
+    // Obtenemos el token biométrico actual
     final biometricToken = await SecureStorageService.getBiometricToken();
+
     if (token == 'default' || biometricToken == 'default') {
       return false;
     }
+
     final url = Uri.http(Config.apiURL, Config.removeBiometric);
-    final body =
-        json.encode({"token": token, "biometricToken": biometricToken});
+
+    final body = json.encode(
+      {"token": token, "biometricToken": biometricToken},
+    );
     try {
-      final response = await http
-          .post(url, headers: headers, body: body)
-          .timeout(const Duration(seconds: 5));
+      final response =
+          await http.post(url, headers: headers, body: body).timeout(
+                const Duration(seconds: 5),
+              );
+
+      // Si la respuesta tiene un status code de 200, borramos el token biométrico y devolvemos true
       if (response.statusCode == 200) {
         await SecureStorageService.clearBiometricToken();
         return true;
@@ -103,30 +134,30 @@ class APIservice{
     }
 
     final url = Uri.http(Config.apiURL, Config.loginBiometric);
-    final headers = {
-      "Access-Control-Allow-Origin": "*",
-      'Content-Type': 'application/json',
-      'Accept': '*/*'
-    };
 
+    //cuerpo de la petición
     final body = json.encode({
       "token": token,
       "biometricToken": biometricToken,
     });
 
     try {
-      final response = await http
-          .post(url, headers: headers, body: body)
-          .timeout(const Duration(seconds: 5));
+      //petición POST
+      final response =
+          await http.post(url, headers: headers, body: body).timeout(
+                const Duration(seconds: 5),
+              );
 
+      //guardamos los detalles del inicio de sesión y devolvemos true
       if (response.statusCode == 200) {
         final loginDetails = loginResponseModel(response.body);
         await Shared.setLogginDetails(loginDetails);
         return true;
       }
     } catch (e) {
-      // fall through
+      print(e);
     }
+
     return false;
   }
 }
--- FrontV2/lib/services/secure.dart
+++ FrontV2/lib/services/secure.dart
@@ -3,31 +3,45 @@ import '../exports.dart';
 class SecureStorageService {
   static late FlutterSecureStorage storage;
 
+  //Se inicia el almacenamiento seguro (toca llamarla en el main)
   static Future<void> setUp() async {
     storage = const FlutterSecureStorage();
   }
 
+// guarda el token biométrico
   static Future<void> setBiometricToken(Map<String, dynamic> json) async {
+    // Obtenemos el token de la respuesta JSON
     String token = json['token'];
+    // Se guarda
     await storage.write(key: "biometricToken", value: token);
   }
 
+// verificar si se está utilizando el token biométrico
   static Future<bool> isUsingBiometric2() async {
+    // lee el valor asociado a la clave 'biometricToken' del almacenamiento seguro
     final response = await storage.read(key: 'biometricToken');
+    // se revisa si hay un valor o no
     return response != null;
   }
 
+// borrar el token biométrico
   static Future<void> clearBiometricToken() async {
     await storage.deleteAll();
   }
 
+// verificar si se está utilizando el token biométrico
   static Future<bool> isUsingBiometric() async {
+    // lee el valor asociado a la clave 'usingBiometric' del almacenamiento seguro
     final response = await storage.read(key: 'usingBiometric');
+    // se revisa si hse utiliza o no
     return response == 'true';
   }
 
+// obtener el token biométrico
   static Future<String> getBiometricToken() async {
+    // lee el valor asociado a la clave 'biometricToken' del almacenamiento seguro
     final token = await storage.read(key: 'biometricToken');
+    // si no hay un valor, devolvemos 'default'
     return token ?? 'default';
   }
 }
