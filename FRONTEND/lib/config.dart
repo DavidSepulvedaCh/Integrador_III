43baa73cb9aa1217921060d77536cd15bd01607c
Daniel Patiño <daniel.patino.2020@upb.edu.co>
2023-04-21 10:29:26 -0500

    Se agregó la ruta getOffersByIdUser

--- FRONTEND/lib/config.dart
+++ FRONTEND/lib/config.dart
@@ -1,13 +1,15 @@
-class Config{
+class Config {
   static const String apiURL = '10.153.50.45:1802';
   static const String loginAPI = '/api/users/login';
   static const String registerAPI = '/api/users/person/register';
   static const String isValidTokenAPI = '/api/users/isValidToken';
-  static const String getRestaurantDetails= '/api/users/restaurant/getInformationByIdUser';
+  static const String getRestaurantDetails =
+      '/api/users/restaurant/getInformationByIdUser';
   static const String getOffers = '/api/offers';
   static const String getOfferById = '/api/offers/getById';
   static const String getOfferByPriceRange = '/api/offers/getByPriceRange';
   static const String updateFavorite = '/api/favorites/updateFavorites';
   static const String getFavorites = '/api/favorites';
   static const String getMaxPriceAllOffers = 'api/offers/getMaxPriceAllOffers';
+  static const String getOffersByIdUser = '/api/offers/getOffersByIdUser';
 }
