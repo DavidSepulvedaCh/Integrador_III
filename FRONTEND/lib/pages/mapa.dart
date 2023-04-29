7cb183948eaa762a70965b6cc982eeaf13189675
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-04-29 17:41:08 -0500

    agregando markets

--- FRONTEND/lib/pages/mapa.dart
+++ FRONTEND/lib/pages/mapa.dart
@@ -2,7 +2,9 @@
 
 import 'dart:async';
 
-import '../routes/imports.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapSample extends StatefulWidget {
   const MapSample({Key? key}) : super(key: key);
@@ -12,40 +14,95 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
+  List<Marker> _markers = [];
+
+// Lista de coordenadas con sus nombres
+  List<Map<String, dynamic>> coordinatesList = [
+    {"name": "Lugar 1", "latitude": 7.119349, "longitude": -73.122742},
+    {"name": "Lugar 2", "latitude": 7.118678, "longitude": -73.119956},
+    {"name": "Lugar 3", "latitude": 7.111902, "longitude": -73.120012},
+  ];
+
+// Crear los marcadores a partir de la lista de coordenadas
+  void _createMarkers() {
+    for (final coordinates in coordinatesList) {
+      final marker = Marker(
+        markerId: MarkerId(coordinates["name"]),
+        position: LatLng(coordinates["latitude"], coordinates["longitude"]),
+        infoWindow: InfoWindow(title: coordinates["name"]),
+      );
+      _markers.add(marker);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _createMarkers();
+  }
+
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  // ignore: prefer_const_declarations
+  static final CameraPosition _bucaramangaPosition = const CameraPosition(
+    target: LatLng(7.119349, -73.122742),
+    zoom: 30,
   );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mapa"),
+        backgroundColor: Colors.deepOrange,
+      ),
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _bucaramangaPosition,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        markers: Set<Marker>.of(_markers),
+        
       ),
-      /* floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ), */
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30, bottom: 15),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: FloatingActionButton(
+            onPressed: _goToUserLocation,
+            backgroundColor: Colors.white,
+            child: const Icon(
+              Icons.my_location,
+              color: Colors.deepOrange,
+            ),
+          ),
+        ),
+      ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToUserLocation() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    LatLng latLng = LatLng(position.latitude, position.longitude);
+    CameraPosition cameraPosition = CameraPosition(target: latLng, zoom: 16);
+    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    setState(() {
+      _markers.clear();
+      _markers.add(
+        Marker(
+          markerId: const MarkerId("Ubicación del usuario"),
+          position: latLng,
+          infoWindow: const InfoWindow(title: "Tu ubicación actual"),
+        ),
+      );
+    });
   }
 }
